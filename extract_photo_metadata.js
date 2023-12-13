91e3acd6064cb952a7fa25a8c4272a7347a27cc9
Christabel05 <christdanquah@gmail.com>
2023-12-13 15:58:30 +0100

    edits

--- extract_photo_metadata.js
+++ extract_photo_metadata.js
@@ -1,48 +1,45 @@
-// importera exifr för att ta metadata från bilderna
+// import file system module
+import fs from 'fs';
 import exifr from 'exifr';
 import mysql from 'mysql2/promise';
-//Importera file system (fs) - inbyggt i node.js
-import fs from 'fs';
- 
-// Ger oss en lista på alla filerna i mappen
+
 let images = fs.readdirSync('images');
- 
-// Kopplar oss till databasen
+
 const db = await mysql.createConnection({
-    // CHANGE TO 127.0.0.1 IF YOU WANT TO RUN LOCAL DB
-    host: '161.97.144.27',
-    port: "8094",
-    user: 'root',
-    password: 'guessagain94',
-    database: 'group4.Metadata'
-  });
- 
-// Funktion för att skapa querys
+  host: '161.97.144.27', 
+  port: '8094',               
+  user: 'root',
+  password: 'guessagain94',  
+  database: 'group4.Metadata'
+});
+
+// A small function for a query
 async function query(sql, listOfValues) {
   let result = await db.execute(sql, listOfValues);
   return result[0];
 }
- 
-// Läs alla filers namn från images mappen
+
+// read all file names from the images fodler
 const files = await fs.readdirSync('images');
- 
-// Loopa genom alla bilder och läs metadata
+
+
+// loop through all images files and read metadata
 for (let image of images) {
- 
-  // Ta metadatan från filen
+
+  // Get the metadata for the file
   let metadata = await exifr.parse('images/' + image);
- 
-  // Sätt in i databasen med hjälp av query funktionen
+
+  // INSERT TO DATABASE
   let result = await query(`
-    INSERT INTO images (fileName, metadata)
+    INSERT INTO photos (photosName, photosDescription)
     VALUES(?, ?)
   `, [image, metadata]);
- 
-  // Logga resultatet för att se att något händer.
+
+  // Log the result of inserting in the database
   console.log(image, result);
- 
+
 }
- 
-// Automatisk stop när det är klart, annars tror VSC
-// att något mer ska skickas in då vi är kopplade till databasen.
+
+// exit/stop the script when everything is imported
+// so you don't have to press Ctrl+C
 process.exit();
